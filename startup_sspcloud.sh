68d03c215560d91471f919d770c82390a173b918
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-05 21:36:17 +0100

    Create a startup file for SSP Cloud

---
+++ startup_sspcloud.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+# Create variables
+WORK_DIR=/home/rstudio/school_meal_forecast_xgboost
+REPO_URL=https://github.com/fBedecarrats/school_meal_forecast_xgboost
+TEMP_DIR=${WORK_DIR}/temp
+
+# Git
+git clone $REPO_URL $WORK_DIR
+chown -R rstudio:users $WORK_DIR
+
+# Folders to store data and documentation
+mkdir $TEMP_DIR
+
+
+# launch RStudio in the right project
+# Copied from InseeLab UtilitR
+    echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && identical(getwd(), path.expand('~')))
+        {
+            message('On charge directement le bon projet :-) ')
+            rstudioapi::openProject('~/school_meal_forecast_xgboost')
+            }
+            }, action = 'append')
+            " >> /home/rstudio/.Rprofile
