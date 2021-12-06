bc8b1eeb13f423f94f98b659c50d979b994c4892
Florent Bédécarrats <f.bedecarrats@gmail.com>
2021-12-06 16:08:37 +0100

    Testing with a python install at startup

---
+++ startup_sspcloud_testpython.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# Est-ce que ça marche d'installer python au démarrage ?
+sudo apt-get update
+sudo apt-get install python3.7.9
+
+
+# Create variables
+WORK_DIR=/home/rstudio/school_meal_forecast_xgboost
+REPO_URL=https://github.com/fBedecarrats/school_meal_forecast_xgboost
+TEMP_DIR=${WORK_DIR}/temp
+
+# Git
+git clone $REPO_URL $WORK_DIR
+chown -R rstudio:users $WORK_DIR
+
+# Folders to store data and documentation
+mkdir $TEMP_DIR
+
+
+# launch RStudio in the right project
+# Copied from InseeLab UtilitR
+    echo \
+    "
+    setHook('rstudio.sessionInit', function(newSession) {
+        if (newSession && identical(getwd(), path.expand('~')))
+        {
+            message('On charge directement le bon projet :-) ')
+            rstudioapi::openProject('~/school_meal_forecast_xgboost')
+            }
+            }, action = 'append')
+            " >> /home/rstudio/.Rprofile
